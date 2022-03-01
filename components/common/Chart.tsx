ee2301a4b8a1bea0ce424871aa4bcd762d20df99
nonolooklook <nonolooklook@users.noreply.github.com>
2022-03-01 03:41:25 +0000

    deploy: onelandworld/oneland-home@4358b38ca42d9ceab7572a819ebcac6c294cddd9

--- components/common/Chart.tsx
+++
@@ -1,6 +0,0 @@
-import ReactECharts, { EChartsReactProps } from "echarts-for-react";
-import { ReactElement } from "react";
-
-export default function Chart(props: EChartsReactProps): ReactElement {
-  return <ReactECharts {...props} />;
-}
